9458e7df44408c4f4e6909e407e2c981a144c8bf
9fin-FE-account <171722889+9fin-FE-account@users.noreply.github.com>
2024-06-21 16:58:11 +0100

    Init template!

---
+++ README.md
@@ -0,0 +1,8 @@
+# 9fin transcripts challenge template
+
+This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules. You can clone this repository and start working on it!
+
+## How to start the dev server
+
+- `npm install`
+- `npm run dev`
---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import logo from './assets/9fin.svg';
+
+function App() {
+  return (
+    <div className='app'>
+      <img src={logo} alt='9fin logo' />
+      <h4>
+        This is a starter repository for the transcripts challenge. Feel free to tinker with any part of the
+        setup to your liking 😎
+      </h4>
+    </div>
+  );
+}
+
+export default App;
